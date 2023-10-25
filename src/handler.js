071b637f594db5c1c8befc76ede41ccf5e57d99f
Alipurnama <Alipurnama69@gmail.com>
2023-10-25 16:13:27 +0700

    Update Finished status book

--- src/handler.js
+++ src/handler.js
@@ -70,19 +70,86 @@ const addBooksHandler = (request, h) => {
 
 //Menampilkan Seluruh Buku
 
+// const getAllBooksHandler = (request, h) => {
+//   const response = h.response({
+//     status: "success",
+//     data: {
+//       books: books.map((book) => ({
+//         id: book.id,
+//         name: book.name,
+//         publisher: book.publisher,
+//       })),
+//     },
+//   });
+//   response.code(200);
+//   return response;
+// };
+
 const getAllBooksHandler = (request, h) => {
-  const response = h.response({
-    status: "success",
-    data: {
-      books: books.map((book) => ({
-        id: book.id,
-        name: book.name,
-        publisher: book.publisher,
-      })),
-    },
-  });
-  response.code(200);
-  return response;
+  const { name, reading, finished } = request.query;
+
+  if (name !== undefined) {
+    const bookname = books.filter((book) =>
+      book.name.toLowerCase().includes(name.toLowerCase())
+    );
+    const response = h.response({
+      status: "success",
+      data: {
+        books: bookname.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  } else if (reading !== undefined) {
+    const BooksReading = books.filter(
+      (book) => Number(book.reading) === Number(reading)
+    );
+
+    const response = h.response({
+      status: "success",
+      data: {
+        books: BooksReading.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  } else if (finished !== undefined) {
+    const BooksFinished = books.filter((book) => book.finished == finished);
+
+    const response = h.response({
+      status: "success",
+      data: {
+        books: BooksFinished.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  } else {
+    const response = h.response({
+      status: "success",
+      data: {
+        books: books.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
 };
 
 //Mendapatkan Detail Buku Berdasarkan ID
